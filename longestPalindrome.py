f22aea5ed2053075f7ecfc9e13b74cdb15fd886c
Sudhanshu Kumar <sudh2810@gmail.com>
2021-01-21 12:11:02 +0530

    daily code

--- longestPalindrome.py
+++ longestPalindrome.py
@@ -1,4 +1,6 @@
 class Solution:
+
+    # time O(n^2) and space O(1)
     def longestPalindrome(self, s: str) -> str:
         
         self.s = s
@@ -29,6 +31,42 @@ class Solution:
             right += 1
             
         return right - left - 1
+
+    # Dynamic programming time O(n^2) and space O(n^2)
+    def longestPalindromeDP(self, s1):
+        
+        if (not s1 or len(s1) < 1): return ''
+        
+        len_ = len(s1)
+        longest = 0
+        res = ''
+        
+        dp = [[False for i in range(len_)] for j in range(len_)]
+        
+        for g in range(len_):
+            
+            i = 0
+            j = g
+            while ( j < len_ ):
+                
+                if (g == 0):
+                    dp[i][j] = True
+                elif (g == 1 and s1[i] == s1[j]):
+                    dp[i][j] = True
+                else:
+                    if ( s1[i]==s1[j] and dp[i+1][j-1] ):
+                        dp[i][j] = True
+                    else:
+                        dp[i][j] = False
+                        
+                if dp[i][j]:
+                    longest = g + 1
+                    res = s1[i:j+1]
+                    
+                i += 1
+                j += 1
+                    
+        return res
         
 
-print(Solution().longestPalindrome('sudnanshu'))
+print(Solution().longestPalindromeDP('sudnanshu'))
