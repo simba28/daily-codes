e6a6a259ebbca43e06959b128ed2660f18d033a3
Sudhanshu Kumar <sudh2810@gmail.com>
2021-04-08 14:46:11 +0530

    daily code

---
+++ checkIfNumberIsSumOfDistinctPowersOfThree.py
@@ -0,0 +1,12 @@
+# link => https://leetcode.com/problems/check-if-number-is-a-sum-of-powers-of-three/
+
+
+class Solution:
+    def checkPowersOfThree(self, n: int) -> bool:
+        while n > 0:
+            if n % 3 == 2:
+                return False
+            else:
+                n //= 3
+
+        return True
