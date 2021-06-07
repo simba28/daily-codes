cc02c5ebc808534bf2d864e5a3ce741273636570
Sudhanshu Kumar <sudh2810@gmail.com>
2021-06-07 23:17:07 +0530

    daily code

--- bestTimeToSellAndBuyStock.py
+++ bestTimeToSellAndBuyStock.py
@@ -5,28 +5,31 @@ If you were only permitted to complete at most one transaction (i.e., buy one an
 one share of the stock), design an algorithm to find the maximum profit.
 '''
 
+
 class Solution:
-    def maxProfit(self, prices) :
-        
+    def maxProfit(self, prices):
+
         profit = 0
         l = len(prices)
-        
-        if not prices: return 0
-        
+
+        if not prices:
+            return 0
+
         min_ = prices[0]
         max_ = prices[0]
-        
+
         i = 1
-        while i<l:
-            if prices[i]<min_:
+        while i < l:
+            if prices[i] < min_:
                 min_ = prices[i]
-                
+
             else:
                 max_ = prices[i]
                 profit = max(profit, max_-min_)
-            
+
             i += 1
-            
+
         return profit
-            
-print(Solution().maxProfit([7,1,5,3,6,4]))
+
+
+print(Solution().maxProfit([7, 1, 5, 3, 6, 4]))
